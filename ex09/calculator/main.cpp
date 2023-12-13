af758e6e89369ad16336a22ba04060beb34c261e
wonyeong2 <qkfka3521@gmail.com>
2023-12-13 18:03:35 +0900

    GTK+ 또는 Qt를 이용한 간단한 계산기 프로그램

---
+++ ex09/calculator/main.cpp
@@ -0,0 +1,11 @@
+#include "calculator.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    Calculator calculator;
+    calculator.show();
+
+    return app.exec();
+}
